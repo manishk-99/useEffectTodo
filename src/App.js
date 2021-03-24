4deb9f1c65524aff1432406bf332f47f6094226e
Manish Kumar <66010479+manishk-99@users.noreply.github.com>
2021-03-24 13:45:37 +0530

    endCommit useEffect-added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { Container } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-export default function App() {
+import Todos from "./Components/Todos";
+import TodoForm from "./Components/TodoForm";
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const localTodos = localStorage.getItem("todos");
+    if (localTodos) {
+      setTodos(JSON.parse(localTodos));
+    }
+  }, []);
+
+  const addTodos = async (todo) => {
+    setTodos([...todos, todo]);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const markComplete = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Container fluid>
+      <h1>Todo with local storage</h1>
+      <Todos todos={todos} markComplete={markComplete} />
+      <TodoForm addTodos={addTodos} />
+    </Container>
   );
-}
+};
+
+export default App;
